044e52488aa1bde17a64372619fa2864de41aba9
Your Name <you@example.com>
2024-03-02 19:17:43 +0100

    Adjust routing to comply with RouterV6

--- src/App.js
+++ src/App.js
@@ -1,29 +1,26 @@
 import { useEffect, useState } from "react";
 import { Route, Routes, useNavigate } from "react-router-dom";
-import Header from "./Header";
-import Nav from "./Nav";
-import Footer from "./Footer";
 import Home from "./Home";
 import NewPost from "./NewPost";
 import PostPage from "./PostPage";
 import About from "./About";
 import Missing from "./Missing";
+import Layout from "./Layout";
 
 function App() {
     const navigate = useNavigate();
     return (
-        <div className="App">
-            <Header />
-            <Nav />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/post" element={<NewPost />} />
-                <Route path="/post/:id" element={<PostPage />} />
-                <Route path="/about" element={<About />} />
+        <Routes>
+            <Route path="/" element={<Layout />}>
+                <Route index element={<Home />} />
+                <Route path="post">
+                    <Route index element={<NewPost />} />
+                    <Route path=":id" element={<PostPage />} />
+                </Route>
+                <Route path="about" element={<About />} />
                 <Route path="*" element={<Missing />} />
-            </Routes>
-            <Footer />
-        </div>
+            </Route>
+        </Routes>
     );
 }
 
